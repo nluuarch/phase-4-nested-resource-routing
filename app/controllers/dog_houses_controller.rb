1e845d7779a824e0470b90c35ba38b10fe4db245
nluuarch@gmail.com <nluuarch@gmail.com>
2022-01-12 21:20:08 -0800

    Completed code-along

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,10 +1,23 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    if params[:dog_house_id]
+      # /dog_houses/:dog_house_id/reviews
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      # /reviews
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
+
 
   private
 
